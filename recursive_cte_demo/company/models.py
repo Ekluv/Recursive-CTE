f71bd01362706867b2ee388fec2d0667120471ae
Ekluv <ekluv@live.com>
2017-07-18 23:23:20 +0530

    Update models.py

--- recursive_cte_demo/company/models.py
+++ recursive_cte_demo/company/models.py
@@ -6,26 +6,18 @@ from cte_tree.models import CTENode
 # Create your models here.
 
 class Employee(CTENode):
-
+    """
+    inherit from CTENode
+    this was an expirement using 3rd party package for CTE queries but doesnt work
+    CTENode gives an extra field `parent` which is self referential foreignkey 
+    like this parent =  models.ForeignKey('self', blank=True, null=True, related_name='children')
+    """
     name = models.CharField(max_length=254)
     joining_date = models.DateField()
     age = models.PositiveIntegerField()
     birthdate = models.DateField()
     address = models.TextField()
     designation = models.CharField(max_length=254)
-    # parent =  models.ForeignKey('self', blank=True, null=True, related_name='children'),
-    # _cte_node_parent = 'manager'
 
     def __str__(self):
         return '%s' % (self.name)
-
-
-
-# WITH recursive children AS (
-#   SELECT id, name
-#   FROM company_employee
-#   UNION
-#   SELECT company_employee.id, company_employee.name
-#   FROM company_employee, children
-#   WHERE company_employee.parent_id = children.id
-# ) select id from TABLE company_employee where id=3;
